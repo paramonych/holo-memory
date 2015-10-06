2351882dad36c83bbb2697f903d74b4b35b26b99
Anton Morozov <Anton.Morozov@Intelsat.com>
2015-10-07 01:54:54 +0600

    Added underscore, created base scene with typical space volume and several randomly positioned neurons branches (and randomly active)

--- src/js/main-scene.js
+++ src/js/main-scene.js
@@ -8,24 +8,72 @@ function initScene() {
     var engine = new BABYLON.Engine(canvas, true);
     var scene = new BABYLON.Scene(engine);
     scene.clearColor = new BABYLON.Color3(.3, .3, .3);
-    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene);
-    camera.setPosition(new BABYLON.Vector3(0, 5, -10));
-    camera.lowerBetaLimit = 0.1;
-    camera.upperBetaLimit = (Math.PI / 2) * 0.99;
-    camera.lowerRadiusLimit = 10;
-    camera.attachControl(canvas, false);
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = .5;
-    light.groundColor = new BABYLON.Color3(.5, .5, .5);
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-    sphere.position.y = 1;
-    createNeuron(scene);
-    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-    engine.runRenderLoop(function () {
-        scene.render();
-    });
+    var scale = 10;
+    attachCamera(canvas, scene, scale);
+    setLight(scene);
+    createNeurons(7, scene, scale);
+    createPatternSpaceBox(scene, scale);
+    engine.runRenderLoop(() => scene.render());
 }
-function createNeuron(scene) {
-    var neuron = BABYLON.Mesh.CreateCylinder("cylinder", 3, 3, 3, 6, 1, scene, false);
+function createNeurons(number, scene, scale) {
+    var neurons = new Array();
+    for (var i = 0; i < number; i++) {
+        neurons.push(createNeuron(scene, scale));
+    }
+}
+function createNeuron(scene, scale) {
+    var neuron = BABYLON.Mesh.CreateCylinder("cylinder", scale * 3, 2 / scale, 2 / scale, scale, 1, scene, false);
+    neuron.position = randomVector(scale);
+    neuron.rotation = randomAngleVector();
+    if (Math.random() > 0.5) {
+        var activeNeuronMaterial = new BABYLON.StandardMaterial("active-neuron", scene);
+        activeNeuronMaterial.emissiveColor = new BABYLON.Color3(1, .9, 0);
+        neuron.material = activeNeuronMaterial;
+    }
     return neuron;
+}
+function randomAngleVector() {
+    var angle = () => randomSign() * Math.PI / Math.random();
+    return new BABYLON.Vector3(angle(), angle(), angle());
+}
+function randomVector(scale) {
+    var num = () => (scale / 2) * Math.random() * randomSign();
+    return new BABYLON.Vector3(num(), num(), num());
+}
+function randomSign() {
+    var a = Math.random();
+    var b = Math.random();
+    return (a - b) / Math.abs(a - b);
+}
+function attachCamera(canvas, scene, scale) {
+    var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 12, 0, scale, new BABYLON.Vector3(0, 0, 0), scene);
+    camera.setPosition(new BABYLON.Vector3(0, scale / 3, -2.5 * scale));
+    var betaLimit = (Math.PI / 2) * 0.99;
+    camera.lowerBetaLimit = 1 / scale;
+    camera.upperBetaLimit = 2 * betaLimit;
+    camera.lowerRadiusLimit = scale / 2;
+    camera.alpha = 5.5;
+    camera.attachControl(canvas, false);
+}
+function setLight(scene) {
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    light.intensity = .8;
+    var lamp = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 50, 50), scene);
+    lamp.intensity = .4;
+    lamp.diffuse = new BABYLON.Color3(0, .1, .1);
+    lamp.specular = new BABYLON.Color3(0, 0, .1);
+}
+function createPatternSpaceBox(scene, scale) {
+    var borderBox = BABYLON.Mesh.CreateBox("borders", scale, scene);
+    borderBox.position = new BABYLON.Vector3(0, 0, 0);
+    var borderBoxMaterial = new BABYLON.StandardMaterial("wire", scene);
+    borderBoxMaterial.wireframe = true;
+    borderBoxMaterial.alpha = 0.3;
+    borderBox.material = borderBoxMaterial;
+    return borderBox;
+}
+function createMediator(scene) {
+    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 2, 2, scene);
+    sphere.position.y = 1;
+    return sphere;
 }
--- src/ts/main-scene.ts
+++ src/ts/main-scene.ts
@@ -18,43 +18,90 @@ function initScene(): void {
   // Change the scene background color to green.
   scene.clearColor = new BABYLON.Color3(.3, .3, .3);
 
-  // This creates and positions a free camera
-  var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene);
-  camera.setPosition(new BABYLON.Vector3(0, 5, -10));
+  var scale = 10;
 
-   camera.lowerBetaLimit = 0.1;
-   camera.upperBetaLimit = (Math.PI / 2) * 0.99;
-   camera.lowerRadiusLimit = 10;
-  // This attaches the camera to the canvas
-  camera.attachControl(canvas, false);
+  attachCamera(canvas, scene, scale);
+  setLight(scene);
+  createNeurons(7, scene, scale);
+  //createMediator(scene);
+  createPatternSpaceBox(scene, scale);
 
-  // This creates a light, aiming 0,1,0 - to the sky.
-  var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-  // Dim the light a small amount
-  light.intensity = .5;
-  light.groundColor = new BABYLON.Color3(.5,.5,.5);
-
-  // Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
-  var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-  // Move the sphere upward 1/2 its height
-  sphere.position.y = 1;
-
-  createNeuron(scene);
-
-  // Let's try our built-in 'ground' shape.  Params: name, width, depth, subdivisions, scene
-  var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-
-  // Register a render loop to repeatedly render the scene
-  engine.runRenderLoop(function () {
-    scene.render();
-  });
+  engine.runRenderLoop(() => scene.render());
 }
 
-function createNeuron(scene: BABYLON.Scene): BABYLON.Mesh {
-  //var path: BABYLON.Vector3[] = ;
-  //var neuron = BABYLON.Mesh.CreateTube('neuron', path, 20, 0, (i, n) => n, 10, scene, true);
-  var neuron = BABYLON.Mesh.CreateCylinder("cylinder", 3, 3, 3, 6, 1, scene, false);
+function createNeurons(number: number, scene: BABYLON.Scene, scale: number): void {
+  var neurons = new Array<BABYLON.Mesh>();
+  for(var i=0; i< number; i++) {
+    neurons.push(createNeuron(scene, scale));
+  }
+}
+
+function createNeuron(scene: BABYLON.Scene, scale: number): BABYLON.Mesh {
+  var neuron = BABYLON.Mesh.CreateCylinder("cylinder", scale*3, 2/scale, 2/scale, scale, 1, scene, false);
+  neuron.position = randomVector(scale);
+  neuron.rotation = randomAngleVector();
+
+  if(Math.random() > 0.5) {
+    var activeNeuronMaterial = new BABYLON.StandardMaterial("active-neuron", scene);
+    activeNeuronMaterial.emissiveColor = new BABYLON.Color3(1, .9, 0);
+
+    neuron.material = activeNeuronMaterial;
+  }
+
   return neuron;
+}
+
+function randomAngleVector(): BABYLON.Vector3 {
+  var angle = () => randomSign()*Math.PI/Math.random();
+  return new BABYLON.Vector3(angle(),angle(),angle());
+}
+
+function randomVector(scale: number): BABYLON.Vector3 {
+  var num = () => (scale/2)*Math.random()*randomSign();
+  return new BABYLON.Vector3(num(),num(),num());
+}
+
+function randomSign(): number {
+  var a = Math.random();
+  var b = Math.random();
+  return (a-b)/Math.abs(a-b);
+}
+
+function attachCamera(canvas: HTMLCanvasElement, scene: BABYLON.Scene, scale: number): void {
+  var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI/12, 0, scale, new BABYLON.Vector3(0, 0, 0), scene);
+
+  camera.setPosition(new BABYLON.Vector3(0, scale/3, -2.5*scale));
+  var betaLimit = (Math.PI / 2) * 0.99;
+  camera.lowerBetaLimit = 1/scale;
+  camera.upperBetaLimit = 2*betaLimit;
+  camera.lowerRadiusLimit = scale/2;
+  camera.alpha = 5.5;
+  camera.attachControl(canvas, false);
+}
+
+function setLight(scene: BABYLON.Scene): void {
+  var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+  light.intensity = .8;
+  //light.groundColor = new BABYLON.Color3(.3,.3,.3);
+
+  var lamp = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 50, 50), scene);
+  lamp.intensity = .4;
+  lamp.diffuse = new BABYLON.Color3(0,.1,.1);
+  lamp.specular = new BABYLON.Color3(0,0,.1);
+}
+
+function createPatternSpaceBox(scene: BABYLON.Scene, scale: number): BABYLON.Mesh {
+  var borderBox = BABYLON.Mesh.CreateBox("borders", scale, scene);
+  borderBox.position = new BABYLON.Vector3(0,0,0);
+  var borderBoxMaterial = new BABYLON.StandardMaterial("wire", scene);
+  borderBoxMaterial.wireframe = true;
+  borderBoxMaterial.alpha = 0.3;
+  borderBox.material = borderBoxMaterial;
+  return borderBox;
+}
+
+function createMediator(scene: BABYLON.Scene): BABYLON.Mesh {
+  var sphere = BABYLON.Mesh.CreateSphere("sphere1", 2, 2, scene);
+  sphere.position.y = 1;
+  return sphere;
 }
